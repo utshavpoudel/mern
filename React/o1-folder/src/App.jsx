7c6d83702ec004c6f20be3276d116b7c64b1b5af
Utshav Poudel <poudelutshav333@gmail.com>
2026-07-18 18:25:46 +0545

    basic structure knowledge

---
+++ React/o1-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+        <h1 id='xyz'>bashant</h1>
+        <h2 id='abc'>utshav</h2>
+      </div>
+
+      <div id="son">
+        <h1 id='cusbc'>hbibeif</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
